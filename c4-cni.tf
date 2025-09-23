09b48416629f5d6ed282ccd63d8ef4e4b8cc555b
BananaSorcery <trantrunghaupro2013@gmail.com>
2025-09-23 18:36:41 +0700

    enable kubernetes provider

--- c4-cni.tf
+++ c4-cni.tf
@@ -12,12 +12,12 @@
 #   ]
 # }
 
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster_windows.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_windows.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.cluster_windows.token
-#   // Ensure the version of the Kubernetes provider you are using does not require `load_config_file`.
-# }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster_windows.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_windows.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster_windows.token
+  // Ensure the version of the Kubernetes provider you are using does not require `load_config_file`.
+}
 
 # # eks is spun up with aws-vpc-cni helm chart regardless if it is specified in cluster_addons
 # # this config can't be set from terraform that I can see. The best option is to overwrite
